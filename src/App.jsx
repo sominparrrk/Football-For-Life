3ae28ccf5aca4df4003c0b7ad1ab9fb35425f57c
sominparrrk <itsmechloe@naver.com>
2021-11-21 02:38:46 +0900

    Feat: Set global style

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/common/Header/Header';
+import GlobalStyle from './styles/GlobalStyle.js';
+
+function App() {
+  return (
+    <div className='App'>
+      <GlobalStyle />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
