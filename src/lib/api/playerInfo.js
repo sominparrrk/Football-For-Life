9213ac0dd53a4c43b3c54948e5b420496dcd299c
sominparrrk <itsmechloe@naver.com>
2022-01-20 19:01:53 +0900

    Feat: Add getPlayerInfo

---
+++ src/lib/api/playerInfo.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const getPlayerInfo = (id) => {
+  const options = {
+    method: 'GET',
+    url: 'https://api-football-beta.p.rapidapi.com/players',
+    params: { season: '2021', team: id.toString(), page: '2' },
+    headers: {
+      'x-rapidapi-host': 'api-football-beta.p.rapidapi.com',
+      'x-rapidapi-key': '2605e9a060msh885fdd0c17d8e72p14b936jsn139f9bd84a62',
+    },
+  };
+
+  try {
+    const data = axios.request(options).then((response) => {
+      return response;
+    });
+    console.log(data);
+  } catch (err) {
+    console.log('[FAIL] getClubs', err);
+  }
+};
+
+export default getPlayerInfo;
