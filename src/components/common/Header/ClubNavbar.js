2e822fb479a037f4d138684fc3fb7088d7ed7560
sominparrrk <itsmechloe@naver.com>
2022-01-20 15:18:39 +0900

    Feat: Change item to real data from API

--- src/components/common/Header/ClubNavbar.js
+++ src/components/common/Header/ClubNavbar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import getClubs from '../../../lib/api/clubs';
 
 const Navbar = styled.nav`
   width: 100%;
@@ -29,43 +30,24 @@ const ClubItem = styled.li`
   }
 `;
 
-const clubDummyData = [
-  { name: 'Liverpool', code: 'LIV' },
-  { name: 'Newcastle', code: 'NEW' },
-  { name: 'Norwich', code: 'NOR' },
-  { name: 'Brentford', code: 'BRE' },
-  { name: 'Everton', code: 'EVE' },
-  { name: 'Leeds', code: 'LEE' },
-  { name: 'Arsenal', code: 'ARS' },
-  { name: 'Aston Villa', code: 'AVL' },
-  { name: 'Leicester', code: 'LEI' },
-  { name: 'Southampton', code: 'SOU' },
-  { name: 'Tottenham', code: 'TOT' },
-  { name: 'Watford', code: 'WAT' },
-  { name: 'Brighton and Hove', code: 'BHA' },
-  { name: 'Man City', code: 'MCI' },
-  { name: 'Man Utd', code: 'MUN' },
-  { name: 'Burnley', code: 'BUN' },
-  { name: 'Chelsea', code: 'CHE' },
-  { name: 'Crystal Palace', code: 'CRY' },
-  { name: 'West Ham', code: 'WHU' },
-  { name: 'Wolves', code: 'WOL' },
-];
-
 // Needs to arrange in alphabetical order
 // .sort() but elements are all object `team.name`
 
 const ClubNavBar = () => {
+  const [clubs, setClubs] = useState([]);
+  useEffect(() => {
+    getClubs().then((clubs) => setClubs(clubs));
+  }, []);
   return (
     <>
       <Navbar>
         <ClubList>
-          {clubDummyData
+          {clubs
             .filter((club, index) => index < 12)
             .map((club) => (
-              <ClubItem key={club.code}>
-                <Link to={`/club/${club.name.replace(/ /gi, '-')}`}>
-                  {club.name}
+              <ClubItem key={club.team.id}>
+                <Link to={`/club/${club.team.name.replace(/ /gi, '-')}`}>
+                  {club.team.name}
                 </Link>
               </ClubItem>
             ))}
---
+++ src/lib/api/clubs.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const getClubs = async () => {
+  const options = {
+    method: 'GET',
+    url: 'https://api-football-beta.p.rapidapi.com/teams',
+    params: { league: '39', season: '2021' },
+    headers: {
+      'x-rapidapi-host': 'api-football-beta.p.rapidapi.com',
+      'x-rapidapi-key': '2605e9a060msh885fdd0c17d8e72p14b936jsn139f9bd84a62',
+    },
+  };
+
+  try {
+    const data = axios.request(options).then((response) => {
+      return response.data.response;
+    });
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log('[FAIL] getClubs', err);
+  }
+};
+
+export default getClubs;
