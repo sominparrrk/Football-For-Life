f7e4b46e1890f0d92f51c113e4d52a2794faec64
sominparrrk <itsmechloe@naver.com>
2022-01-23 22:51:35 +0900

    Feat: Block scroll when modal opens

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { IconPath } from '../../../lib/staticPathData';
 
@@ -36,6 +36,19 @@ const CloseIcon = styled.img.attrs({
 `;
 
 const Modal = ({ title, embed }) => {
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed;
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;
+    `;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
   const videoURL = embed.slice(
     embed.indexOf('https'),
     embed.lastIndexOf(`' frameborder`)
